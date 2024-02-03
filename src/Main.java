1b5ff38cee558df4d4406febc8cfb4c3cac1a8b0
FURAHA MAXWELL EGESA <146840417+Simply-Furaha@users.noreply.github.com>
2024-02-03 08:45:40 +0300

    If statement for Grading

--- src/Main.java
+++ src/Main.java
@@ -1,4 +1,4 @@
-import java.util.Arrays;
+
 import java.util.Scanner;
 
 // Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
@@ -9,7 +9,10 @@ public class Main {
         Scanner input = new Scanner(System.in);
         System.out.println("Enter your grade: ");
         Grade = input.next().charAt(0);
-
+        int marks;
+       Scanner scan= new Scanner(System.in);
+        System.out.println("Enter your marks: ");
+        marks = scan.nextInt();
      switch (Grade) {
         case 'A':
             System.out.println("Excellent");
@@ -29,6 +32,21 @@ public class Main {
             default:
             System.out.println("Invalid Grade");
             break;
+        }
+        if (marks >= 90 && marks <= 100) {
+            System.out.println("A");
+        } else if (marks >= 80 && marks < 90) {
+            System.out.println("B");
+        } else if (marks >= 70 && marks < 80) {
+            System.out.println("C");
+        } else if (marks >= 60 && marks < 70) {
+            System.out.println("D");
+        } else {
+            System.out.println("F");
+        }
+
+        if (marks != Grade) {
+            System.out.println("Invalid Grade");
         }
     }
 }
